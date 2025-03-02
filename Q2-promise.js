ccea060ea108e5f1a891e55edac16f6c70a6721f
mresean8615 <seanpayne479@gmail.com>
2025-03-02 15:15:31 -0700

    Q2-promise was not added for some reason

--- Q2-promise.js
+++ Q2-promise.js
@@ -1,1 +1,34 @@
-// Write the code for Question 2b here
+const fs = require("fs/promises");
+
+const runAsyncFunctions = async () => {
+  // write to file
+  try {
+    await fs.writeFile("working.txt", "Creating new file...");
+  } catch (err) {
+    console.error(err);
+  }
+
+  // append to file
+  try {
+    await fs.appendFile("working.txt", "\nHeyo");
+  } catch (err) {
+    console.error(err);
+  }
+
+  // read file
+  try {
+    const data = await fs.readFile("working.txt", "utf8");
+    console.log(data.toString());
+  } catch (err) {
+    console.error(err);
+  }
+
+  // rename file
+  try {
+    await fs.rename("working.txt", "complete.txt");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+runAsyncFunctions();
